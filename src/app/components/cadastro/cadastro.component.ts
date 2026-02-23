41a3db7f6e65e3100b40f234f8ab76e894c43d38
guilherme-alc <guilhermealc01@gmail.com>
2026-02-23 20:34:26 -0300

    Ajusta formulário de cadastro e adiciona date picker

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,11 +1,14 @@
+import { Cliente, createCliente } from '../../models/cliente';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker'
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ClienteService } from '../../services/cliente.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -17,10 +20,19 @@ import { MatIconModule } from '@angular/material/icon';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule],
+    MatIconModule,
+    MatDatepickerModule
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
-export class CadastroComponent {
 
+export class CadastroComponent {
+  protected cliente: Cliente = createCliente();
+
+  constructor (private service: ClienteService) { }
+
+  protected salvarCliente() {
+    this.service.salvar(this.cliente);
+  }
 }
