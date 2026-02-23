0b837ce4d6907a0f864e9027961d336f0ff521c3
guilherme-alc <guilhermealc01@gmail.com>
2026-02-22 21:10:30 -0300

    Cria formulário de cadastro + estilização

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    FormsModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
