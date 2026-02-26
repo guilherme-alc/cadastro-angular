c6358865c6bafd2a66c5daf6af9c915a9654a0b9
guilherme-alc <guilhermealc01@gmail.com>
2026-02-25 22:36:11 -0300

    feat: adiciona validações de campos no cadastro e feedbacks de ação

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,6 +1,10 @@
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgForm, FormsModule } from '@angular/forms';
 import { Cliente, createCliente } from '../../models/cliente';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ClienteService } from '../../services/cliente.service';
+import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
+import { MatNativeDateModule } from '@angular/material/core';
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field'
@@ -8,31 +12,57 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker'
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { ClienteService } from '../../services/cliente.service';
 
 @Component({
   selector: 'app-cadastro',
+  standalone: true,
   imports: [
+    CommonModule,
     FormsModule,
     FlexLayoutModule,
     MatInputModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule,
     MatButtonModule,
     MatIconModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatSnackBarModule,
+    MatNativeDateModule
   ],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.scss'
+  styleUrls: ['./cadastro.component.scss']
 })
 
 export class CadastroComponent {
-  protected cliente: Cliente = createCliente();
+  public cliente: Cliente = createCliente();
+  @ViewChild('clientesFrm') clientesFrm?: NgForm;
+  constructor (private service: ClienteService, private snackBar: MatSnackBar) { }
 
-  constructor (private service: ClienteService) { }
+  public salvarCliente() {
+    try {
+      this.service.salvar(this.cliente);
+      this.cliente = createCliente();
 
-  protected salvarCliente() {
-    this.service.salvar(this.cliente);
+      this.clientesFrm?.resetForm(this.cliente);
+      this.snackBar.open('Cliente salvo com sucesso.', undefined, {
+        duration: 3500,
+        horizontalPosition: 'end',
+        verticalPosition: 'top'
+      });
+    } catch (e: any) {
+      if (e && e.message === 'Email já cadastrado') {
+        this.snackBar.open('Este e-mail já está cadastrado.', 'Fechar', {
+          duration: 5000,
+          horizontalPosition: 'end',
+          verticalPosition: 'top'
+        });
+      } else {
+        this.snackBar.open('Erro ao salvar cliente. Veja console.', 'Fechar', {
+          duration: 5000,
+          horizontalPosition: 'end',
+          verticalPosition: 'top'
+        });
+      }
+    }
   }
 }
